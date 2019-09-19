0adca738ad1841e1f1c5ce473cb7ed5a9afe754a
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-09-19 08:34:47 +0200

    add redux store

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import './App.scss';
 import { Header } from './components/layout';
 import { Dashboard } from './components/leads';
 
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App: FunctionComponent = () => {
   return (
-    <Fragment>
-      <Header/>
-      <div className="container">
-        <Dashboard/>
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    </Provider>
   );
-}
+};
 
 export default App;
